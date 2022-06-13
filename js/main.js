f8ec475d0c28a75bdfee1a9ff02ccfb72ab1652c
Matheus Chiodi <matheuschiodi20@gmail.com>
2022-06-13 13:45:39 -0300

    Total

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const elementoResposta = document.querySelector("#resposta")
+const inputPergunta = document.querySelector("#inputPergunta")
+const buttonPerguntar = document.querySelector('#buttonPerguntar')
+const respostas = [
+    "Certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Pergunte novamente mais tarde.",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Só sei que nada sei.",
+    "Melhor não te dizer agora.",
+    "A meu ver, sim.",
+    "Até que o sol não brilhe.",
+    "Provavelmente.",
+    "Não é possível prever agora.",
+    "A força não provém da capacidade física.",
+    "As perspectivas não são tão boas.",
+    "É um animal de linguagem.",
+    "Concentre-se e pergunte novamente.",
+    "Sinais apontam que sim.",
+]
+
+// clicar em fazer pergunta
+function fazerPergunta() {
+
+  if(inputPergunta.value == "") {
+    alert("Digite sua pergunta")
+    return
+  }
+
+  buttonPerguntar.setAttribute("disabled", true)
+
+  const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+  // gerar numero aletorio
+  const  totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+  elementoResposta.style.opacity = 1;
+
+  // sumir a resposta depois de 3 segundos
+  setTimeout(function() {
+    elementoResposta.style.opacity = 0;
+    buttonPerguntar.removeAttribute("disabled")
+  }, 3000)
+}
